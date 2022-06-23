8defc02c2ef9c6cf3044efaeb218adfca9f77616
christianesl <christianesl@gmail.com>
2022-06-23 13:20:29 -0500

    adding loop

--- Publisher.py
+++ Publisher.py
@@ -5,8 +5,11 @@ channel = connect.channel()
 
 channel.queue_declare(queue="mna29")
 
+x = 1
+while x < 5:
+	msg=input("Enter your message: ")
+	channel.basic_publish(exchange='', routing_key='mna29', body=msg)
+	print("sent :", msg)
+	x += 1
 
-msg=input("Enter your message: ")
-channel.basic_publish(exchange='', routing_key='mna29', body=msg)
-print("sent :", msg)
 connect.close()
